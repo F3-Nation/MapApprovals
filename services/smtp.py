152367b85d17782d83a9d1a1809d88424d57051c
Damon <damon.vinciguerra@gmail.com>
2024-01-18 13:19:09 -0700

    trying to get smtp working in GCP

--- services/smtp.py
+++ services/smtp.py
@@ -1,13 +1,20 @@
 import os
 import smtplib
+import ssl
 from email.message import EmailMessage
 
 class SMTP:
-    _server = smtplib.SMTP('smtp.gmail.com', 587)
-    _server.starttls()
-    _server.login('map.admin@f3nation.com', 'Wxa74L9Bcp^B^pFe')
+    _EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
+
     
     def send_email(self, subject: str, toEmails: list, body: str) -> None:
+
+        context = ssl.create_default_context()
+
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
+        server.starttls(context=context)
+        server.login('map.admin@f3nation.com', self._EMAIL_PASSWORD)
 
         message = EmailMessage()
         message['Subject'] = subject
@@ -15,4 +22,6 @@ class SMTP:
         message['To'] = ', '.join(toEmails)
         message.set_content(body, subtype='html')
 
-        self._server.send_message(message)
+        server.send_message(message)
+
+        server.close()

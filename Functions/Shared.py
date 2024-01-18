1f30126a2db4ea2e311668c99c609badf145380c
Damon <damon.vinciguerra@gmail.com>
2024-01-18 09:00:13 -0700

    went back to old smtp setting (doesn't work while on VPN)

--- Functions/Shared.py
+++ Functions/Shared.py
@@ -3,8 +3,7 @@ from email.message import EmailMessage
 
 # Email
 def sendEmail(subject: str, toEmails: list, body: str) -> None:
-    #smtp = smtplib.SMTP('smtp.gmail.com', 587)
-    smtp = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+    smtp = smtplib.SMTP('smtp.gmail.com', 587)
     smtp.starttls()
     smtp.login('map.admin@f3nation.com', 'Wxa74L9Bcp^B^pFe')
 
